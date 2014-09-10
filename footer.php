a886277dbcc1d12ad13205c88eb04d20ef6795af
franzwcom <github@franzw.com>
2014-09-10 19:37:52 +0200

    added headers, footers, etc a lot

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+|--------------------------------------------------------------------------
+| the footer
+|--------------------------------------------------------------------------
+|
+| 
+| 
+| 
+|
+*/
+ ?>
+
+ 
+   
+<!-- Latest compiled and minified Jquery  -->
+ 	<script src="<?php bloginfo('template_directory');?>/bower_components/jquery/dist/jquery.min.js"></script>
+ 	<!-- Latest compiled and minified Bootstrap JavaScript -->
+ 	<script src="<?php bloginfo('template_directory');?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+ 	<!-- master js file -->
+ 	<script src="<?php bloginfo('template_directory');?>/js/main.min.js"></script>
+ 			
+ </body>
+ </html>
+
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+|--------------------------------------------------------------------------
+| the header
+|--------------------------------------------------------------------------
+|
+| 
+| 
+| 
+|
+*/
+ ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"/> <!-- prepares the device for determined sizes -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico" type="image/x-icon">
+    <title>wordpress boilerplate</title>
+    <!-- Latest compiled and minified BootstrapCSS -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/master.css">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('template_directory');?>/images/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('template_directory');?>/images/apple-touch-icon-114-precomposed.png">
+       <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('template_directory');?>/images/apple-touch-icon-72-precomposed.png">
+                     <link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_directory');?>/images/apple-touch-icon-57-precomposed.png">
+                                                            <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+  
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
