b506a1af645c80671a7077e874dd2fde297f2eb5
Franz DSKTP <github@franzw.com>
2014-09-14 01:47:22 +0200

    worked with the style of every post

--- content.php
+++ content.php
@@ -16,27 +16,40 @@
  ?>
 
 
- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+ <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-8'); ?>>
      
-  <div class="row">
-      <div class="col-md-12">
-          <h1> <a class="letitle" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-          <?php 
-             // check if the post has a Post Thumbnail assigned to it.
-              if ( has_post_thumbnail() ) {
-                the_post_thumbnail('thumbnail', array('class' => 'img-thumbnail img-responsive alignleft'));
-              } 
-           ?>
-          <p><?php the_content(); ?></p>
-          <hr>
-           Author: <p> <a href="<?php get_the_author_link(); ?>"><?php the_author(); ?></a> </p> <br>
-          <p> Categories: <?php the_category(', '); ?></p> <br>
-           <p><?php the_tags(); ?></p> <br>
-           <p>Other posts by <?php the_author_posts_link(); ?></p> <br>
-           <hr>
-      </div>
+   
 
-  </div>
+
+
+        <div class="panel panel-default">
+              <div class="panel-heading">
+                <a class="letitle panel-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div>
+              
+              <div class="panel-body">
+                  <?php 
+                       // check if the post has a Post Thumbnail assigned to it.
+                        if ( has_post_thumbnail() ) {
+                          the_post_thumbnail('thumbnail', array('class' => 'img-thumbnail img-responsive alignleft'));
+                        } 
+                  ?>
+                  <p><?php the_content(); ?></p>
+                     <hr>
+                     Author: <p> <a href="<?php get_the_author_link(); ?>"><?php the_author(); ?></a> </p>
+                     <p> Categories:</p><h3><span class="label label-default le-label"><?php the_category(', '); ?></span></h3> 
+                     <h3><span class="label label-primary le-label"><?php the_tags(); ?></span></h3><br>
+                     <p>Other posts by <?php the_author_posts_link(); ?></p>
+                     <hr>
+                     <?php comments_template(); ?> 
+                     
+              </div>
+          </div>
+
+
  
 
  </article>
+
+
+
--- index.php
+++ index.php
@@ -16,25 +16,25 @@
 
    <?php //get_template_part( 'nav' );   ?>
 
- <div class="main-content col-md-8" role="main">
- 	
 
  
 
 		 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		    <?php  get_template_part('content'); ?>
+		    <?php  get_template_part('content', get_post_format() ); ?>
               
 
-		 <?php endwhile; else:?>
+		 <?php endwhile;?>
 
-		    <?php _e('&lsquo; No posts were found. sorry !!' ); ?>
+		  <?php else: ?>
+
+		    <?php  get_template_part('content', 'none' ); ?>
 
 		 <?php endif; ?>
 
- <?php comments_template(); ?> 
+ 
 
- </div> <!-- end "main-content col-md-8" role="main" -->
+
 
 
  <?php get_footer(); // the footer ?>
