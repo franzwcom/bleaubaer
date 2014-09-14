f888cd10982dc0c675886ad46621ece366da3d3d
Franz DSKTP <github@franzw.com>
2014-09-14 12:04:14 +0200

    added new files

--- page.php
+++ page.php
@@ -13,11 +13,15 @@
 
  <?php get_header() ?>
 
- <?php while( have_posts() ) : the_post(); ?>
- 	<article id="post-<?php the_ID(); ?> <?php post_class(); ?>">
-
- 	</article>
- <?php endwhile; ?>
+	 <?php while( have_posts() ) : the_post(); ?>
+	 	<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12'); ?> >
+           <h1><?php the_title(); ?></h1>
+           <?php the_content(); ?>
+           
+           <?php wp_link_pages(); ?>
+	 	</article>
+	 	   <?php comments_template(); ?> 
+	 <?php endwhile; ?>
 
 
  <?php get_footer(); ?>
